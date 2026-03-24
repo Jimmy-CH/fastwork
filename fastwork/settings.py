b40cb70eb30b4d9a11f63ec4ed45d63080a74bc7
陈光 <03420092@yto.net.cn>
2026-03-24 09:36:57 +0800

    feat:apps

--- fastwork/settings.py
+++ fastwork/settings.py
@@ -37,6 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # app
+    'accounts',
+    'customer',
+    'service_order',
+    'product',
+    'warehouse',
+    'mall',
+    'ai_chatbot',
+    'message_center',
+    'knowledge_base',
+    'system_management',
 ]
 
 MIDDLEWARE = [
@@ -103,12 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'zh-hans'
+TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = True
-
 USE_TZ = True
 
 
